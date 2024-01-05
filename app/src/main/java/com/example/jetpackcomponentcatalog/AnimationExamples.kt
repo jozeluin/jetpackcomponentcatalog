7d4a63f44e31fed2b99c439c8580c378187f893a
jozel <jozeluin@gmail.com>
2024-01-05 18:17:29 +0100

    Curso pC , AnimacionColorBasica

---
+++ app/src/main/java/com/example/jetpackcomponentcatalog/AnimationExamples.kt
@@ -0,0 +1,45 @@
+package com.example.jetpackcomponentcatalog
+
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ColorAnimationSimple() {
+    Column() {
+
+        var firstColor by rememberSaveable {
+            mutableStateOf(false)
+        }
+        var realColor = if (firstColor) Color.Red else Color.Yellow
+        Box(modifier = Modifier
+            .size(100.dp)
+            .background(realColor)
+            .clickable { firstColor = !firstColor })
+
+        Spacer(modifier = Modifier.size(200.dp))
+
+
+        var secondColor by rememberSaveable {
+            mutableStateOf(false)
+        }
+        val realColor2 by animateColorAsState(targetValue = if (secondColor) Color.Red else Color.Yellow)
+        Box(modifier = Modifier
+            .size(100.dp)
+            .background(realColor2)
+            .clickable { secondColor = !secondColor })
+    }
+
+}
