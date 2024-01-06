969a018b5ecbb89c17d5c782f3dfd78bcc39c251
jozel <jozeluin@gmail.com>
2024-01-06 12:33:21 +0100

    Curso pC , AnimacionColorAvanzada

--- app/src/main/java/com/example/jetpackcomponentcatalog/AnimationExamples.kt
+++ app/src/main/java/com/example/jetpackcomponentcatalog/AnimationExamples.kt
@@ -1,11 +1,10 @@
 package com.example.jetpackcomponentcatalog
 
 import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -18,28 +17,26 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun ColorAnimationSimple() {
-    Column() {
 
-        var firstColor by rememberSaveable {
-            mutableStateOf(false)
-        }
-        var realColor = if (firstColor) Color.Red else Color.Yellow
-        Box(modifier = Modifier
-            .size(100.dp)
-            .background(realColor)
-            .clickable { firstColor = !firstColor })
-
-        Spacer(modifier = Modifier.size(200.dp))
-
-
-        var secondColor by rememberSaveable {
-            mutableStateOf(false)
-        }
-        val realColor2 by animateColorAsState(targetValue = if (secondColor) Color.Red else Color.Yellow)
-        Box(modifier = Modifier
-            .size(100.dp)
-            .background(realColor2)
-            .clickable { secondColor = !secondColor })
+    var firstColor by rememberSaveable {
+        mutableStateOf(false)
     }
+
+    var showBox by rememberSaveable {
+        mutableStateOf(true)}
+
+    val realColor by animateColorAsState(
+        targetValue = if (firstColor) Color.Red else Color.Yellow,
+        label = "Etiqueta Descriptiva",
+        animationSpec = tween(durationMillis = 2000),
+        finishedListener = {showBox=false}
+    )
+if(showBox) {
+    Box(modifier = Modifier
+        .size(100.dp)
+        .background(realColor)
+        .clickable { firstColor = !firstColor })
+}
+
 
 }
